507ff534759736690e4840504e43e677fdeb00fa
Kai <kaiserzhao@tencent.com>
2021-01-10 22:29:51 +0800

    update arcface: adaptive margin

--- vlkit/ops/nn.py
+++ vlkit/ops/nn.py
@@ -38,21 +38,27 @@ def deconv_upsample(channels, stride, fixed=True):
     return upsample
 
 class ArcFace(nn.Module):
-    """ 
+    """
     ArcFace https://arxiv.org/pdf/1801.07698
     """
-    def __init__(self, in_features, out_features, s=32, m=0.5):
+    def __init__(self, in_features, out_features, s=32, m=0.5, ada_m=False,
+                 warmup_iters=-1, return_m=False):
         super(ArcFace, self).__init__()
         self.weight = nn.Parameter(torch.zeros(out_features, in_features))
-        self.s = s 
-        self.m = m 
+        self.s = s
+        self.m = m
         self.cos_m = math.cos(self.m)
         self.sin_m = math.sin(self.m)
+
+        self.ada_m = ada_m
+        self.warmup_iters = warmup_iters
+        self.return_m = return_m
+        self.iter = 0
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.xavier_uniform_(self.weight)
+        nn.init.normal_(self.weight, mean=0, std=0.01)
 
     def forward(self, input, label=None):
 
@@ -61,20 +67,38 @@ class ArcFace(nn.Module):
         if label is None or self.m == 0:
             return cosine
 
+        if self.ada_m:
+            self.iter = self.iter + 1
+            if self.iter < self.warmup_iters:
+                m = (1 - math.cos((math.pi / self.warmup_iters) * self.iter)) / 2 * self.m
+            else:
+                m = self.m
+            self.cos_m = math.cos(m)
+            self.sin_m = math.sin(m)
+        else:
+            m = self.m
+
         # sin(theta)
-        sine = torch.sqrt(1.0 - torch.pow(cosine, 2)) 
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
 
         # psi = cos(theta + m)
-        psi_theta = cosine*self.cos_m - sine*self.sin_m
-        psi_theta = torch.where(cosine > -self.cos_m, psi_theta, -psi_theta-2)
+        psi_theta = cosine * self.cos_m - sine * self.sin_m
+        psi_theta = torch.where(cosine > -self.cos_m, psi_theta, -psi_theta - 2)
 
         one_hot = torch.zeros_like(cosine)
         one_hot.scatter_(1, label.view(-1, 1).long(), 1)
 
         output = (one_hot*psi_theta + (1-one_hot)*cosine) * self.s
 
-        return output, cosine.detach() * self.s
+        if self.return_m:
+            return output, cosine.detach() * self.s, m
+        else:
+            return output, cosine.detach() * self.s
 
     def __str__(self):
         return "ArcFace() in_features=%d out_features=%d s=%.3f m=%.3f" % \
                (self.weight.shape[1], self.weight.shape[0], self.s, self.m)
+    def __repr__(self):
+        return self.__str__()
+    def extra_repr(self):
+        return self.__str__()
