e19bc20eb530338e2be1ce9384e3309058eb33ec
Kai <kaiserzhao@tencent.com>
2021-01-18 23:36:55 +0800

    load/save pickle

--- vlkit/io.py
+++ vlkit/io.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 from os.path import isfile
 from .image import isimg
+import pickle
 
 def imread(path, backend="pil", grayscale=False):
     assert isfile(path) and isimg(path)
@@ -25,3 +26,13 @@ def imwrite(im, path, backend="pil"):
         cv2.imwrite(path, im)
     else:
         raise ValueError("Invalid backend %s. (Valid options are ['pil', 'cv2'])" % backend)
+
+def pickle_save(object, path):
+    f = open(path, "wb")
+    pickle.dump(object, f)
+    f.close()
+    return path
+
+def pickle_load(path):
+    f = open(path, "rb")
+    return pickle.load(f)
